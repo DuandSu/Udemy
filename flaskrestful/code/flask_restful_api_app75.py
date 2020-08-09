3f7b6e9516ffb290753d1c66f49a663c7ec78b5d
DuandSu <duane.w.munro@gmail.com>
2020-08-09 00:27:26 -0600

    C240 9-Aug-2020: Udemy: Complete Section 4.

---
+++ flaskrestful/code/flask_restful_api_app75.py
@@ -0,0 +1,68 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # creates new end point /auth
+items = []
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        # could use list instead of next if more than one item in list.
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # ternary operator.
+        # return {'item': item}, 200 if item is not None else 404
+        # shortened to
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        # arguments handy are force=True, but can be dangerous, or silent=True
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+        )
+        data = parser.parse_args()
+        
+        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        #Udemy Video did reverse with "if item is None:"
+        if item:
+            # item['price'] = data['price']
+            item.update(data)
+        else:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+            
+        return item, 201
+        
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+# port 5000 is default so argument not really necessary.
+app.run(port=5000, debug=True)
