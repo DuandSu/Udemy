5d2d15ab62842e2534b9e3c232aabab1f6c575e8
DuandSu <duane.w.munro@gmail.com>
2020-08-16 14:19:03 -0600

    Udemy RAwFaP 16-Aug-2020: Sec 6 Separate pre sqlalchemy.

---
+++ sqlalchemy/code/app97.py
@@ -0,0 +1,25 @@
+from db import db
+
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # creates new end point /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+# port 5000 is default so argument not really necessary.
+if __name__ == '__main__':
+    db_init_app(app)
+    app.run(port=5000, debug=True)
